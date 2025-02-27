81ce2dc4a6a58dd3958e0b68945d2de9e10d53fb
qingli <1175641066@qq.com>
2025-02-27 16:24:47 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -2,6 +2,22 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl= 'https://api-hmugo-web.itheima.net'
+//请求拦截器
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title:'数据加载中.....'
+  })
+  
+}
+//响应拦截器
+$http.afterRequest= function(options){
+  uni.hideLoading()
+  
+}
 
 Vue.config.productionTip = false
 
@@ -16,6 +32,29 @@ app.$mount()
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl= 'https://api-hmugo-web.itheima.net'
+//请求拦截器
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title:'数据加载中.....'
+  }),
+  uni.$showMsg = function(){
+    uni.showToast({
+      title:"数据请求成功！",
+      duration:1500,
+      icon:"none"
+    })
+  }
+  
+}
+//响应拦截器
+$http.afterRequest= function(options){
+  uni.hideLoading()
+  
+}
 export function createApp() {
   const app = createSSRApp(App)
   return {
